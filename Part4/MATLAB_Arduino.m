4373a4b6f4e570da785e5997b68e972289e9dccc
Arman-Goshtasbi <143842138+Arman-Goshtasbi@users.noreply.github.com>
2023-09-12 14:18:30 +0200

    Update MATLAB_Arduino.m

--- Part4/MATLAB_Arduino.m
+++ Part4/MATLAB_Arduino.m
@@ -1,20 +1,25 @@
+
+
 %% Setting up arudino
-a = arduino('com','Uno');
+a = arduino('COM7','Uno');
 
 
 %% Collecting voltage information from the arduino
-seconds =10; % Defining the required duration for the data collection
+seconds =5; % Defining the required duration for the data collection
 elapse_time = 0;
 tic % initiate time duration
-
+v = [];
 while elapse_time<seconds
-    v = readVoltage(a,'A0'); % collecting voltage data from Arduino.
+    v_new = readVoltage(a,'A1'); % collecting voltage data from Arduino.
+    v = [v;v_new];
     % Please mind that you have to adjust the pin based on the your input
     % pin in arduino
     elapse_time = toc; %collecting time for each iteration 
+    pause(0.05)
 end
 
 
 %% Applying sensor transfer function 
 
  P = (v-0.2)/0.045; % calculating the pressure based on the read voltage
+    
